08183839cc0bb130c4572d7e167ca0be2e98cfe2
jezes70 <jezes70@gmail.com>
2024-02-13 02:55:35 +0100

    init

---
+++ src/test/java/com/decagon/eventhubbe/controller/EventControllerTest.java
@@ -0,0 +1,44 @@
+package com.decagon.eventhubbe.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventControllerTest {
+
+    @Test
+    void createEvent() {
+    }
+
+    @Test
+    void activateEvent() {
+    }
+
+    @Test
+    void findAllEvents() {
+    }
+
+    @Test
+    void findAllEventsByUser() {
+    }
+
+    @Test
+    void findAllEventsByCategory() {
+    }
+
+    @Test
+    void findAllEventsByKeyword() {
+    }
+
+    @Test
+    void getEventById() {
+    }
+
+    @Test
+    void updateEvent() {
+    }
+
+    @Test
+    void deleteEvent() {
+    }
+}
